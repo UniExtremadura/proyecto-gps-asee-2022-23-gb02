6614cade91a96e255a96b78b530b265960424d2f
Carlos <cacasquer@alumnos.unex.es>
2022-11-19 17:06:44 +0100

    //Añadir los layouts de las clases

--- app/src/main/java/es/unex/propuesta_proyecto/ClasesActivity.java
+++ app/src/main/java/es/unex/propuesta_proyecto/ClasesActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -14,7 +15,8 @@ import java.util.ArrayList;
 public class ClasesActivity extends AppCompatActivity {
 
     RecyclerView rvClases;
-    ArrayList<String> alClases = new ArrayList<String>();;
+    ArrayList<String> alClases = new ArrayList<String>();
+    private Button botonClase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +39,56 @@ public class ClasesActivity extends AppCompatActivity {
         alClases.add("Clase 1");
         alClases.add("Clase 2");
         alClases.add("Clase 3");
+        alClases.add("Clase 4");
+        alClases.add("Clase 5");
+        alClases.add("Clase 6");
+        alClases.add("Clase 7");
+        alClases.add("Clase 8");
+        alClases.add("Clase 9");
+        alClases.add("Clase 10");
 
         ClasesAdapter clasesAdapter = new ClasesAdapter(alClases);
         rvClases.setAdapter(clasesAdapter);
-
         // Acceso a clases
+        botonClase = rvClases.findViewById(R.id.bClase);
 
     }
 // Mirar onclickListener (1 para cada clase (1 diseño para cada clase))
     public void accederClases(View view){ // TODO - Acceso a las diferentes clases
-        Intent actClase = new Intent(this,DetalleClaseActivity.class);
+        Intent actClase = new Intent(this, DetalleClaseActivity.class);
+        /*String buttonText = botonClase.getTag().toString();
+        switch (buttonText){
+            case "Clase 2":
+                actClase.putExtra("className", 2);
+                break;
+            case "Clase 3":
+                actClase.putExtra("className", 3);
+                break;
+            case "Clase 4":
+                actClase.putExtra("className", 4);
+                break;
+            case "Clase 5":
+                actClase.putExtra("className", 5);
+                break;
+            case "Clase 6":
+                actClase.putExtra("className", 6);
+                break;
+            case "Clase 7":
+                actClase.putExtra("className", 7);
+                break;
+            case "Clase 8":
+                actClase.putExtra("className", 8);
+                break;
+            case "Clase 9":
+                actClase.putExtra("className", 9);
+                break;
+            case "Clase 10":
+                actClase.putExtra("className", 10);
+                break;
+            default:
+                actClase.putExtra("className", 1);
+                break;
+        }*/
         startActivity(actClase);
     }
 
--- app/src/main/java/es/unex/propuesta_proyecto/DetalleClaseActivity.java
+++ app/src/main/java/es/unex/propuesta_proyecto/DetalleClaseActivity.java
@@ -11,8 +11,42 @@ public class DetalleClaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalle_clases);
-
+        Bundle parametros = this.getIntent().getExtras();
+        if(parametros != null) {
+            int valor = parametros.getInt("className");
+            switch (valor) {
+                case 2:
+                    setContentView(R.layout.activity_detalle_clase2);
+                    break;
+                case 3:
+                    setContentView(R.layout.activity_detalle_clase3);
+                    break;
+                case 4:
+                    setContentView(R.layout.activity_detalle_clase4);
+                    break;
+                case 5:
+                    setContentView(R.layout.activity_detalle_clase5);
+                    break;
+                case 6:
+                    setContentView(R.layout.activity_detalle_clase6);
+                    break;
+                case 7:
+                    setContentView(R.layout.activity_detalle_clase7);
+                    break;
+                case 8:
+                    setContentView(R.layout.activity_detalle_clase8);
+                    break;
+                case 9:
+                    setContentView(R.layout.activity_detalle_clase9);
+                    break;
+                case 10:
+                    setContentView(R.layout.activity_detalle_clase10);
+                    break;
+                default:
+                    setContentView(R.layout.activity_detalle_clases);
+                    break;
+            }
+        }
 
     }
 
